53cf309d268856390d14a14d58eeff130f7176fd
Prateek Arora <prateekksw@gmail.com>
2016-04-06 16:48:11 +0530

    project setup

---
+++ app/src/main/java/android/com/soundrolling/Widget/CustomTextView.java
@@ -0,0 +1,39 @@
+package android.com.soundrolling.Widget;
+
+import android.com.soundrolling.R;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: deepaksingh
+ * Date: 12/6/13
+ * Time: 12:02 PM
+ * To change this template use File | File Templates.
+ */
+public class CustomTextView extends TextView {
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+        if (isInEditMode()) {
+            return;
+        }
+
+        TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.CustomTextView);
+        String fontName = styledAttrs.getString(R.styleable.CustomTextView_typeface);
+        styledAttrs.recycle();
+
+        if (fontName != null) {
+            Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+            setTypeface(typeface);
+        }
+        this.setPaintFlags(this.getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+    }
+
+}
